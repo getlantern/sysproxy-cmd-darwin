1a06e66d0c719567316ffba7bab150511b7d80e0
Myles Horton <myles@getlantern.org>
2022-09-02 20:27:32 -0400

    cleanup

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 set -euo pipefail
 
+out=../sysproxy-cmd/binaries/darwin/sysproxy
 xcodebuild build
 codesign --options runtime --strict --timestamp --force --deep -r="designated => anchor trusted and identifier com.getlantern.lantern" -s "Developer ID Application: Innovate Labs LLC (4FYC28AXA2)" -v build/Release/sysproxy
-cp build/Release/sysproxy ../sysproxy-cmd/darwin
-file ../sysproxy-cmd/darwin
+cp build/Release/sysproxy $out
+file $out
